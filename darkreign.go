66e9e03cf482bc203733980195dd2d41e3e6cfa2
Armin Schlegel <armin.schlegel@conplement.de>
2019-05-31 09:45:51 +0200

    Initial commit. Starting the game from local files working

---
+++ darkreign.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/zserge/webview"
+)
+
+func startServer() string {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatalf("Error http.listen: %v", err)
+	}
+	fs := http.FileServer(http.Dir("app/"))
+	http.Handle("/app/", http.StripPrefix("/app/", fs))
+
+	go func() {
+		defer ln.Close()
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Open app/game.html to start the game")
+		})
+		log.Fatal(http.Serve(ln, nil))
+	}()
+
+	return "http://" + ln.Addr().String()
+}
+
+func main() {
+	url := startServer() + "/app/game.html"
+	fmt.Println("Serving game at: " + url)
+	w := webview.New(webview.Settings{
+		Width:  640,
+		Height: 480,
+		Title:  "Dark Reign",
+		URL:    url,
+	})
+	defer w.Exit()
+	w.Run()
+}
